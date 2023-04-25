8fe22c878a9b6827a6f2d76badd631dd0038f976
muz2002 <114134934+muz2002@users.noreply.github.com>
2023-04-25 18:53:38 +0600

    Add files via upload

---
+++ EnhancedTipCalculator/src/main/java/com/example/enhancedtipcalculator/EnhancedTipCalculator.java
@@ -0,0 +1,24 @@
+package com.example.enhancedtipcalculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.scene.Parent;
+
+public class EnhancedTipCalculator extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("EnhancedTipCalculator.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.setTitle("Enhanced Tip Calculator");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
